aaaa478b4566e5513aa37f436045c097e7a1d818
Jon Rhymes <jonrhymes@Alisons-MacBook-Air.local>
2020-06-12 12:12:19 -0400

    json webtoken .js added

---
+++ backend/jwt.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+SECRET = 'Shipperific';
+
+userdata = {
+    username: 'Bryce',
+    id: '12345'
+};
+
+// CREATE / SIGN
+
+const token = jwt.sign(userdata, SECRET);
+console.log(token);
+
+// VERIFY
+try {
+    const payload = jwt.verify(token, SECRET);
+    console.log(payload);
+    const bad = jwt.verify(token, 'bad');
+    console.log(bad)
+} catch(error) {
+    console.log(error)
+}
